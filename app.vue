41ea964815f3cb081267cf58baa12ee63de589e7
vuquyet808 <vuquyet808@gmail.com>
2023-10-18 16:05:52 +0700

    feat: test page

--- app.vue
+++ app.vue
@@ -1,5 +1,56 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="w-44 h-44 hidden xl:block absolute left-16 top-12">
+      <NuxtImg src="/pngwing.webp" />
+    </div>
+    <div
+      class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-3 lg:py-16 lg:px-6"
+    >
+      <div
+        class="lg:col-span-2 font-light text-gray-500 sm:text-lg dark:text-gray-400"
+      >
+        <h2
+          class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
+        >
+          🎄 Trang Trí Cây Thông Noel Đón Ngày Giáng Sinh 🎄
+        </h2>
+        <p class="mb-4 pt-6">
+          Ngày Giáng Sinh đã đến, và giờ đây là thời điểm hoàn hảo để biến cây
+          thông Noel thành tâm điểm ấn tượng trong ngày này. Dưới đây là một số
+          gợi ý trang trí thú vị:
+        </p>
+
+        <!-- <p class="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+          id nisi.
+        </p> -->
+      </div>
+      <div class="flex justify-center items-center mt-8">
+        <NuxtImg src="/pngwing.com.webp" />
+      </div>
+    </div>
+  </section>
+  <footer class="footer p-10 bg-slate-950 text-neutral-content">
+    <nav>
+      <header class="footer-title">Services</header>
+      <a class="link link-hover">Branding</a>
+      <a class="link link-hover">Design</a>
+      <a class="link link-hover">Marketing</a>
+      <a class="link link-hover">Advertisement</a>
+    </nav>
+    <nav>
+      <header class="footer-title">Company</header>
+      <a class="link link-hover">About us</a>
+      <a class="link link-hover">Contact</a>
+      <a class="link link-hover">Jobs</a>
+      <a class="link link-hover">Press kit</a>
+    </nav>
+    <nav>
+      <header class="footer-title">Legal</header>
+      <a class="link link-hover">Terms of use</a>
+      <a class="link link-hover">Privacy policy</a>
+      <a class="link link-hover">Cookie policy</a>
+    </nav>
+  </footer>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    '@nuxtjs/robots',
+    'nuxt-simple-sitemap',
+    '@nuxt/image',
+  ],
+  robots: { configPath: '~/config/robots.config.js', rules: {} },
+  image: {
+    quality: 80,
+    format: ['webp'],
+    screens: {
+      'xs': 320,
+      'sm': 640,
+      'md': 768,
+      'lg': 1024,
+      'xl': 1280,
+      'xxl': 1536,
+      '2xl': 1536
+    },
+    dir: 'public/images'
+
+  }
 })
